a3d9ce9dd4a3c07aabb5e10c2600e06372d86a55
isasouza23 <isabellesouza659@gmail.com>
2023-08-04 15:11:42 -0300

    ate_string
    Até as string

---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,9 @@
+console.log("Meu nome é Isabelle.");
+console.log(`Desenvolvimento é no Senai`);
+
+// Template literais (crase) ao lado da tecla P.
+// Usar shift
+console.log(`teste`);
+
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
