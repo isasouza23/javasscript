b4cc52560f1de896582762d9e4d7d92e024df11f
isasouza23 <isabellesouza659@gmail.com>
2023-11-16 16:18:12 -0300

    nike exercicio

--- exemplos/10c-Eventos/eventos.js
+++ exemplos/10c-Eventos/eventos.js
@@ -11,7 +11,15 @@ const sub01 = pagina.querySelector("#subtitulo-exemplo01");
 
 
 // 1ª Digitação (Aqui)
+// Função para mostrar a frase (Quando o mouse sobre)
+sub01.addEventListener('mouseover', function(){
+    msg.textContent = 'Você passou o mouse sobre...';
+});
 
+// Função quando o mouse sai 
+sub01.addEventListener('mouseout', function(){
+    msg.textContent = '';
+});
 
 
 // ____________________________________________________________________________
@@ -20,7 +28,15 @@ const sub01 = pagina.querySelector("#subtitulo-exemplo01");
 
 
 // 2ª Digitação (Aqui)
-
+const botao = pagina.querySelector("#noturno");
+botao.addEventListener('click', function() {
+    pagina.classList.toggle('modo-noturno');
+    if (botao.textContent == 'Ativar') {
+        botao.textContent = 'Desativar';
+    } else {
+        botao.textContent = 'Ativar'
+    }
+});
 
 
 // ____________________________________________________________________________
@@ -65,8 +81,17 @@ formulario.addEventListener('submit', function(event) {
 
 
 // 3ª Digitação (Aqui)
+function calculaMedia(nota1 , nota2) {
+    return (nota1 + nota2) / 2;
+} 
 
-
+function verificaSituacao(media) {
+    return 'Aprovado';
+    if (media >=7) {
+    } else {
+        return 'Reprovado';
+    }
+}
 
 
 function montaResumo(nome, media, situacao) {
--- exemplos/Calculadora dark/script.js
+++ exemplos/Calculadora dark/script.js
@@ -1,1 +1,46 @@
 /* Digite o JS aqui */
+//Seleciona o elemento HTML com o id 'inputBox' e armazena-o na variável 'input'.
+let input = document.getElementById('inputBox');
+
+//Seleciona todos os elementos HTML do tipo 'button' e armazena-os em 'buttons'.
+let buttons = document.querySelectorAll('button');
+
+// Inicializa uma string vazia para armazenar a entrada do usuário.
+let string = "";
+//
+let arr = Array.from(buttons);
+
+//Percorre sobre cada botão no array 'arr'.
+arr.forEach(button => {
+    // Adiciona um ouvinte de eventos de clique a cada botão
+    button.addEventListener('click', (e) => {
+        // Verifica se o texto dentro do botão clicado é '='
+        if (e.target.innerHTML == '=') {
+                  // A função eval() é uma função JavaScript que avalia e executa dinamicamente o código. Quando a lonha é executada, o JavaScript interpreta o conteúdo de string como uma expressão e a avalia. No exemplo dado, eval("2+2") resultaria em 4, porque ele calcula a soma
+            string = eval(string);
+             // Define o valor da caixa de netrada 'input' como a nova string calculada
+            input.value = string;
+
+    // Verfica se o texto dentro do botão clicado é 'AC' (Clear All).
+        }
+        else if (e.target.innerHTML == 'AC') {
+             // Limpa a string, redefininfo-a como uma string vazia.
+            string = "";
+        // Define o valor da caixa de entrada 'input' como uma string vazia.
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+              // Remove o último caractere da string
+            string = string.substring(0, 
+                string.length - 1);
+              // Define o valor da caixa de entrada 'input' como a nova string após a remoção.
+            input.value = string;
+        }
+        else {
+            // Adiciona o textp do botão clicado à string de entrada.
+            string += e.target.innerHTML;
+            // Define o valor da caixa de entrada 'input' como a nova string de entrada.
+            input.value = string;
+        }
+    })
+})
